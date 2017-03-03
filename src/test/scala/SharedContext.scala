8e0e6ee019992fd10a9837b9eb63976eb9a4a0b6
firas <firas.fbs.1989@gmail.com>
2017-03-03 10:55:31 +0100

    CNAM-168-refactor
    shared context

--- src/test/scala/SharedContext.scala
+++ src/test/scala/SharedContext.scala
@@ -4,13 +4,15 @@ import java.io.File
 import java.util.{Locale, TimeZone}
 import org.apache.commons.io.FileUtils
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark._
-import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.sql.hive.test.TestHiveSingleton
-import org.scalatest._
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpecLike, Suite}
 
-abstract class SharedContext extends FlatSpecLike with BeforeAndAfterAll with BeforeAndAfterEach
-  with TestHiveSingleton { self: Suite =>
+/**
+  * Created by sathiya on 15/02/17.
+  */
+abstract class SharedContext extends FlatSpecLike with BeforeAndAfterAll with BeforeAndAfterEach {
+  self: Suite =>
 
   Logger.getRootLogger.setLevel(Level.WARN)
   Logger.getLogger("org").setLevel(Level.WARN)
@@ -20,8 +22,24 @@ abstract class SharedContext extends FlatSpecLike with BeforeAndAfterAll with Be
   Locale.setDefault(Locale.US)
   TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
 
-  override val sqlContext: HiveContext = hiveContext
-  lazy val sc: SparkContext = sqlContext.sparkContext
+  private var _spark: SparkSession = _
+
+  protected def spark: SparkSession = _spark
+  protected def sqlContext: SQLContext = _spark.sqlContext
+  protected def sc: SparkContext = _spark.sparkContext
+
+  protected override def beforeAll(): Unit = {
+    if(_spark == null) {
+      _spark = SparkSession
+        .builder()
+        .appName("Tests")
+        .master("local[4]")
+        .config("spark.sql.testkey", "true")
+        .getOrCreate()
+    }
+
+    super.beforeAll()
+  }
 
   override def beforeEach(): Unit = {
     FileUtils.deleteDirectory(new File("target/test/output"))
@@ -29,7 +47,14 @@ abstract class SharedContext extends FlatSpecLike with BeforeAndAfterAll with Be
   }
 
   override def afterAll() {
-    FileUtils.deleteDirectory(new File("target/test/output"))
-    super.afterAll()
+    try {
+      if(_spark != null) {
+        _spark.stop()
+        _spark = null
+      }
+    } finally {
+      FileUtils.deleteDirectory(new File("target/test/output"))
+      super.afterAll()
+    }
   }
 }
