6b608737df0d2f4cd3a9c08642b88fed26393a77
waterponey <prosper.burq@gmail.com>
2017-03-17 13:02:24 +0100

    CNAM-169 cleaning groupByKey code

--- src/main/scala/fr/polytechnique/cmap/cnam/utilities/CollectionTool.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/utilities/CollectionTool.scala
@@ -5,23 +5,10 @@ package fr.polytechnique.cmap.cnam.utilities
   */
 object CollectionTool {
 
-
-  /*implicit class groupableMap[T](map:List[(String, T)]) {
-
-    def groupByKey(): Map[String, List[T]] = {
-      map.groupBy(_._1).map{
-        case(key, valueList) =>
-          (key, valueList.map(_._2))
-      }
-    }
-  }*/
-
   implicit class groupableMap[K, V](it: Iterable[(K, V)]) {
 
     def groupByKey: Map[K, List[V]] = {
-      it.groupBy(_._1).map {
-        case (key, valueList) => (key, valueList.map(_._2).toList)
-      }
+      it.groupBy(_._1).mapValues(_.map(_._2).toList)
     }
   }
 
