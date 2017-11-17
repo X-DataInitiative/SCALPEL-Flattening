219cad1a718d04ed76b0359fb7d5f372e003c435
firas <firas.fbs.1989@gmail.com>
2017-11-17 14:54:50 +0100

    CNAM 208 add logs
    also change schema for DCIR
    CNAM 208 add logs
    
    
    CNAM 208 recover config
    
    
    CNAM 208 Add logs
    
    
    CNAM 208 adding some metrics
    
    remove flattening config changes
    CNAM 208 minor fixes
    
    
    CNAM-208 review comment

--- src/main/scala/fr/polytechnique/cmap/cnam/flattening/FlatTable.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/flattening/FlatTable.scala
@@ -2,6 +2,8 @@ package fr.polytechnique.cmap.cnam.flattening
 
 import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode}
 import com.typesafe.config.Config
+import fr.polytechnique.cmap.cnam.flattening.FlatteningMain.logger
+import org.apache.log4j.{Level, Logger}
 
 class FlatTable(sqlContext: SQLContext, config: Config) {
 
@@ -29,14 +31,37 @@ class FlatTable(sqlContext: SQLContext, config: Config) {
   }
 
   val joinFunction: (DataFrame, DataFrame) => DataFrame = (accumulator, tableToJoin) => {
-      accumulator.join(tableToJoin, foreignKeys, "left_outer")
+    accumulator.join(tableToJoin, foreignKeys, "left_outer")
   }
 
-  def writeAsParquet: Unit = flatTablePerYear
-    .foreach(
-      joinedTable =>
-        joinedTable.df
-          .write
-          .mode(SaveMode.Overwrite)
-          .parquet(outputBasePath + "/" + joinedTable.name))
+  def writeAsParquet: Unit = {
+    Logger.getRootLogger.setLevel(Level.ERROR)
+    val logger: Logger = Logger.getLogger(getClass)
+    logger.setLevel(Level.INFO)
+
+    flatTablePerYear
+      .foreach {
+        joinedTable =>
+          logger.info("starting join for : " + joinedTable.name)
+          val t0 = System.nanoTime()
+
+          val flatTable = joinedTable.df.persist()
+
+          val nbLinesFlatTable = flatTable.count()
+          logger.info("   Number of lines : " + nbLinesFlatTable)
+
+          val t1 = System.nanoTime()
+          logger.info("   Flattening Duration " + (t1 - t0)/Math.pow(10,9) + " sec")
+
+          flatTable.write
+            .mode(SaveMode.Overwrite)
+            .parquet(outputBasePath + "/" + joinedTable.name)
+          flatTable.unpersist()
+          val t2 = System.nanoTime()
+          logger.info("   writing duration " +joinedTable.name + (t2 - t1)/Math.pow(10,9) + " sec")
+      }
+    }
+
+
+
 }
