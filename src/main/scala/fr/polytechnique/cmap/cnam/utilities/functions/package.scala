0fb92d239b6e2a7b36d8ddb1b51f612360bbbbcb
VU SAINTONGE KEVIN (CNAM / Paris) <VUSAINTONGE-30401@cnamts.local>
2019-04-12 10:08:19 +0200

    Report

--- src/main/scala/fr/polytechnique/cmap/cnam/utilities/functions/package.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/utilities/functions/package.scala
@@ -1,49 +1,11 @@
 package fr.polytechnique.cmap.cnam.utilities
 
 import java.sql.Timestamp
-import java.text.{ParsePosition, SimpleDateFormat}
-import scala.reflect.runtime.universe.TypeTag
-import org.apache.spark.sql.catalyst.ScalaReflection
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{StructType, TimestampType}
-import org.apache.spark.sql.{Column, Dataset}
 
 
 package object functions {
 
-  def classToSchema[CaseClass : TypeTag](): StructType = {
-    ScalaReflection.schemaFor[CaseClass].dataType.asInstanceOf[StructType]
-  }
-
-  def computeDateUsingMonthYear(month: Column, year: Column): Column = {
-    unix_timestamp(
-      concat(lit("1"), lit("-"), month, lit("-"), year),
-      "dd-MM-yyyy"
-    ).cast(TimestampType)
-  }
-
   class DateParseException(val msg: String) extends Exception(msg)
-
-  def parseTimestamp(value: String, pattern: String = "yyyy-MM-dd HH:mm:ss.S"): Option[Timestamp] = {
-
-    if (value == null || value.trim == "") return None
-
-    val dateFormat = new SimpleDateFormat(pattern)
-    // Only exact patterns should be accepted
-    // https://docs.oracle.com/javase/8/docs/api/java/text/DateFormat.html#setLenient-boolean-
-    dateFormat.setLenient(false)
-
-    Option(dateFormat.parse(value.trim, new ParsePosition(0))) match {
-      case None => throw new DateParseException(
-        s"""Cannot parse Timestamp with pattern "$pattern" from the string value "$value""""
-      )
-      case Some(date: java.util.Date) => Some(new Timestamp(date.getTime))
-    }
-  }
-
-  def parseDate(value: String, pattern: String = "yyyy-MM-dd"): Option[java.sql.Date] = {
-    parseTimestamp(value, pattern).map(ts => new java.sql.Date(ts.getTime))
-  }
 
   def makeTS(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0): Timestamp = {
     if(!year.between(1000, 3000) || !month.between(1, 12) || !day.between(1,31) ||
@@ -61,12 +23,6 @@ package object functions {
     case List(year, month, day, hour, minute, second) => makeTS(year, month, day, hour, minute, second)
     case _ => throw new IllegalArgumentException("Illegal Argument List for makeTS function")
   }
-
-  def daysBetween(end: Timestamp, start: Timestamp): Double = {
-    (end.getTime - start.getTime) / (24.0 * 3600.0 * 1000.0)
-  }
-
-  def unionDatasets[A](datasets: Dataset[A]*): Dataset[A] = datasets.reduce(_.union(_))
 
   implicit class IntFunctions(num: Int) {
     def between(lower: Int, upper: Int): Boolean = num >= lower && num <= upper
